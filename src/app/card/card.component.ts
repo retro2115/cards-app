ec67538d261c82495fad04c3ab8d65be1438ff7f
Mirza Cerim <genuineoutburst@gmail.com>
2023-10-01 14:48:05 +0200

    update

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,5 +6,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./card.component.css']
 })
 export class CardComponent {
-
+    posts = [
+      {
+        title: 'Neat tree',
+        imageUrl: 'assets/tree.jpeg',
+        username: 'nature',
+        content: 'I saw this neat tree today'
+      },
+      {
+        title: 'Snowy mountain',
+        imageUrl: 'assets/mountain.jpeg',
+        username: 'mountainlower',
+        content: 'Here is a picture of a snowy mountain'
+      },
+      {
+        title: 'mountain biking',
+        imageUrl: 'assets/biking.jpeg',
+        username: 'niking1222',
+        content: 'I didi some biking today'
+      }
+      
+    ]
 }
