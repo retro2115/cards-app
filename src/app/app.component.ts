4fc7d6cc0a6ee4c4506f63cdea172afd4a6ddb44
Mirza Cerim <retro2115@outlook.com>
2023-10-02 14:09:51 +0200

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,25 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'cards-app';
+
+  posts = [
+	{
+	  title: 'Neat tree',
+	  imageUrl: 'assets/tree.jpeg',
+	  username: 'nature',
+	  content: 'I saw this neat tree today'
+	},
+	{
+	  title: 'Snowy mountain',
+	  imageUrl: 'assets/mountain.jpeg',
+	  username: 'mountainlower',
+	  content: 'Here is a picture of a snowy mountain'
+	},
+	{
+	  title: 'mountain biking',
+	  imageUrl: 'assets/biking.jpeg',
+	  username: 'niking1222',
+	  content: 'I did some biking today'
+	}
+  ]
 }
