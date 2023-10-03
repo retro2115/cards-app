8f933b0d5cedc56b90492f73173411f44384fb4e
Mirza Cerim <genuineoutburst@gmail.com>
2023-10-03 02:58:50 +0200

    update

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class CardComponent {
 
- 
-    
+  @Input() title="";
+  @Input() imageUrl="";
+  @Input() username="";
+  @Input() content="";
+   
 }
